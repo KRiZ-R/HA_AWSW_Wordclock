8d2d413ada4443b411431580622da651f07419af
b1u3n4zgu1 <171537+bluenazgul@users.noreply.github.com>
2024-12-01 12:41:02 +0100

    Update switch.py

--- custom_components/awsw_wordclock/switch.py
+++ custom_components/awsw_wordclock/switch.py
@@ -1,4 +1,3 @@
-
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.config_entries import ConfigEntry
@@ -9,30 +8,100 @@ import logging
 
 LOGGER = logging.getLogger(__name__)
 
-GERMAN_WORDS = {
-    1: "ALARM",
-    2: "GEBURTSTAG",
-    3: "MÜLL RAUS BRINGEN",
-    4: "AUTO",
-    5: "FEIERTAG",
-    6: "FORMEL1",
-    7: "GELBER SACK",
-    8: "URLAUB",
-    9: "WERKSTATT",
-    10: "ZEIT ZUM ZOCKEN",
-    11: "FRISEUR",
-    12: "TERMIN"
+LANGUAGE_WORDS = {
+    "German": {
+        1: "ALARM",
+        2: "GEBURTSTAG",
+        3: "MÜLL RAUS BRINGEN",
+        4: "AUTO",
+        5: "FEIERTAG",
+        6: "FORMEL1",
+        7: "GELBER SACK",
+        8: "URLAUB",
+        9: "WERKSTATT",
+        10: "ZEIT ZUM ZOCKEN",
+        11: "FRISEUR",
+        12: "TERMIN",
+    },
+    "English": {
+        1: "ALARM",
+        2: "BIRTHDAY",
+        3: "TAKE OUT TRASH",
+        4: "CAR",
+        5: "HOLIDAY",
+        6: "FORMULA1",
+        7: "YELLOW BAG",
+        8: "VACATION",
+        9: "WORKSHOP",
+    },
+    "Dutch": {
+        1: "ALARM",
+        2: "VERJAARDAG",
+        3: "AFVAL BUITEN",
+        4: "AUTO",
+        5: "VAKANTIE",
+        6: "FORMULE1",
+        7: "GELE ZAK",
+        8: "WERKPLAATS",
+        9: "VRIJE TIJD",
+    },
+    "French": {
+        1: "ALARME",
+        2: "ANNIVERSAIRE",
+        3: "SORTIR LES DÉCHETS",
+        4: "VOITURE",
+        5: "VACANCES",
+        6: "FORMULE1",
+        7: "SAC JAUNE",
+        8: "ATELIER",
+        9: "TEMPS LIBRE",
+    },
+    "Italian": {
+        1: "ALLARME",
+        2: "COMPLEANNO",
+        3: "PORTARE FUORI LA SPAZZATURA",
+        4: "AUTO",
+        5: "VACANZA",
+        6: "FORMULA1",
+        7: "SACCO GIALLO",
+        8: "OFFICINA",
+        9: "TEMPO LIBERO",
+    },
+    "Swedish": {
+        1: "LARM",
+        2: "FÖDELSEDAG",
+        3: "TA UT SOPOR",
+        4: "BIL",
+        5: "SEMESTER",
+        6: "FORMEL1",
+        7: "GUL PÅSE",
+        8: "VERKSTAD",
+        9: "FRITID",
+    },
+    "Spanish": {
+        1: "ALARMA",
+        2: "CUMPLEAÑOS",
+        3: "SACAR LA BASURA",
+        4: "COCHE",
+        5: "VACACIONES",
+        6: "FÓRMULA1",
+        7: "BOLSA AMARILLA",
+        8: "TALLER",
+        9: "TIEMPO LIBRE",
+    },
 }
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
     """Set up WordClock switches from a config entry."""
     ip_address = entry.data["ip_address"]
+    language = entry.data.get("language", "German")
     device_id = f"wordclock_{ip_address.replace('.', '_')}"
     switches = []
 
-    LOGGER.info("Setting up WordClock switches for IP: %s", ip_address)
+    LOGGER.info("Setting up WordClock switches for IP: %s with language: %s", ip_address, language)
 
-    for word_id, word_name in GERMAN_WORDS.items():
+    words = LANGUAGE_WORDS.get(language, {})
+    for word_id, word_name in words.items():
         switches.append(WordClockSwitch(ip_address, word_id, word_name, device_id))
 
     async_add_entities(switches)
