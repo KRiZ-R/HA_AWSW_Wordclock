b96eb6c9feeeb44c8b47af6cd1e21cfdd988ef35
b1u3n4zgu1 <171537+bluenazgul@users.noreply.github.com>
2024-12-01 12:44:28 +0100

    Update config_flow.py

--- custom_components/awsw_wordclock/config_flow.py
+++ custom_components/awsw_wordclock/config_flow.py
@@ -1,7 +1,16 @@
-
 from homeassistant import config_entries
 import voluptuous as vol
 from .const import DOMAIN
+
+LANGUAGES = {
+    "German": "Deutsch",
+    "English": "English",
+    "Dutch": "Nederlands",
+    "French": "Français",
+    "Italian": "Italiano",
+    "Swedish": "Svenska",
+    "Spanish": "Español",
+}
 
 class WordClockConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for AWSW WordClock."""
@@ -14,15 +23,35 @@ class WordClockConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             ip_address = user_input.get("ip_address")
             if self._is_valid_ip(ip_address):
-                return self.async_create_entry(
-                    title=f"WordClock ({ip_address})", data={"ip_address": ip_address}
-                )
+                self.ip_address = ip_address
+                return await self.async_step_language()
             errors["base"] = "invalid_ip"
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
                 vol.Required("ip_address"): str
+            }),
+            errors=errors
+        )
+
+    async def async_step_language(self, user_input=None):
+        """Handle the step for selecting language."""
+        errors = {}
+        if user_input is not None:
+            selected_language = user_input.get("language")
+            return self.async_create_entry(
+                title=f"WordClock ({self.ip_address})",
+                data={
+                    "ip_address": self.ip_address,
+                    "language": selected_language,
+                },
+            )
+
+        return self.async_show_form(
+            step_id="language",
+            data_schema=vol.Schema({
+                vol.Required("language", default="German"): vol.In(LANGUAGES.keys())
             }),
             errors=errors
         )
