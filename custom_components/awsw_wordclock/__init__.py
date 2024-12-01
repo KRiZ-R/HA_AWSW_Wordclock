c7acc2cba7d39641af3c189612f76c210df07684
b1u3n4zgu1 <171537+bluenazgul@users.noreply.github.com>
2024-12-01 13:44:45 +0100

    Update __init__.py

--- custom_components/awsw_wordclock/__init__.py
+++ custom_components/awsw_wordclock/__init__.py
@@ -7,6 +7,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up AWSW WordClock from a config entry."""
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    # Register an update listener for options
+    entry.add_update_listener(update_options)
+
     await hass.config_entries.async_forward_entry_setups(entry, ["switch"])
     return True
 
@@ -16,3 +20,8 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
     return unload_ok
+
+async def update_options(hass: HomeAssistant, config_entry: ConfigEntry):
+    """Update options if changed."""
+    hass.data[DOMAIN][config_entry.entry_id] = config_entry.data
+
